1a2f0954a655b91fa5d423e4b797c73724f09fd3
Treven Womack <keytonwomack@yahoo.com>
2020-01-06 12:35:07 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -33,8 +33,9 @@ def apply_coupons(cart, coupons)
  counter = 0 
  while counter = coupons.length
  cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
- couponed_item_name = #{coupons[counter][:item]} w/coupon
- cart_item
+ couponed_item_name = "#{coupons[counter][:item]}w/coupon"
+ cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+ if cart_item 
 end
 
 def apply_clearance(cart)
